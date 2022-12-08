94bbf9c2318351222760679aad6f9797e36e1e39
Silvery86 <giang8692@gmail.com>
2022-12-08 23:21:47 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT,function () {
     console.log("Sever is running...");
