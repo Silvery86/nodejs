c7096e7b82627a077804e79cb0c6c0d76870382f
Silvery86 <giang8692@gmail.com>
2022-12-10 02:09:55 +0700

    update

--- index.js
+++ index.js
@@ -50,3 +50,55 @@ app.get("/g3-insurance-by-productid",function (req,res){
     }
   })
 });
+
+app.get("/g3-get-companies",function (req,res){
+  const sql = "select * from group3_companies";
+  conn.query(sql, function (err,data){
+    if (err){
+      res.send("404 not found");
+    }else{
+      res.send(data);
+    }
+  })
+});
+
+app.get("/g3-insurance-by-company",function (req,res){
+  const companyId = req.query.companyId;
+  const sql = `SELECT * FROM group3_insuranceproducts A 
+                left join group3_companies B on A.companyId = B.companyId 
+                 left join group3_vehicles C on A.vehicleId = C.vehicleId 
+                 WHERE A.companyId = ${companyId};`;
+  conn.query(sql, function (err,data){
+    if (err){
+      res.send("404 not found");
+    }else{
+      res.send(data);
+    }
+  })
+});
+
+app.get("/g3-get-vehicles",function (req,res){
+  const sql = "select * from group3_vehicles";
+  conn.query(sql, function (err,data){
+    if (err){
+      res.send("404 not found");
+    }else{
+      res.send(data);
+    }
+  })
+});
+
+app.get("/g3-insurance-by-vehicle",function (req,res){
+  const vehicleId = req.query.vehicleId;
+  const sql = `SELECT * FROM group3_insuranceproducts A 
+                left join group3_companies B on A.companyId = B.companyId 
+                 left join group3_vehicles C on A.vehicleId = C.vehicleId 
+                 WHERE A.vehicleId = ${vehicleId};`;
+  conn.query(sql, function (err,data){
+    if (err){
+      res.send("404 not found");
+    }else{
+      res.send(data);
+    }
+  })
+});
